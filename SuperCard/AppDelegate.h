cc1e926a932816defa62c7a5019fe428ae650dc3
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-17 14:58:42 +0300

    Initial Commit

---
+++ SuperCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Igor on 3/17/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Igor on 3/17/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
